23807b4cd68cc12ddf4315c4309a4b498435f7e0
Giorgio <shadigiorgioislam@gmail.com>
2021-01-12 17:28:12 +0100

    Add freccette effect nella selezione lingua

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
+  $('i.fa-angle-down').addClass('active');
   $('li').click(function(){
-    $('li .box').removeClass('active');
+    $('li').not(this).children('.box').removeClass('active');
     $(this).children('.box').toggleClass('active');
+    if ($('.box.lang').hasClass('active')) {
+      $('i.fa-angle-down').removeClass('active');
+      $('i.fa-angle-up').addClass('active');
+    }
+    else {
+      $('i.fa-angle-up').removeClass('active');
+      $('i.fa-angle-down').addClass('active');
+    }
   });
 
 
